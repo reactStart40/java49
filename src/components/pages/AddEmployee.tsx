723c893cb6b50ae8846b88263f0f6a24bace2c6f
Olga Greyskop <o.greyskop@gmail.com>
2023-02-22 21:30:58 +0200

    cw#44 bigining

--- src/components/pages/AddEmployee.tsx
+++ src/components/pages/AddEmployee.tsx
@@ -3,12 +3,10 @@ import {Box, Button} from "@mui/material";
 import {useDispatch} from 'react-redux';
 import { employeesActions } from '../../redux/employees-slice';
 import { createRandomEmployee } from '../../service/EmployeesService';
+import { EmployeeForm } from '../forms/EmployeeForm';
 export const AddEmployee: React.FC = () => {
     const dispatch = useDispatch();
-    return <Box>
-        <Button onClick={() => 
-            dispatch(employeesActions.addEmployee(createRandomEmployee()))}>
-                Add Random Employee
-            </Button>
-    </Box>
+    return <EmployeeForm submitFn={(employee)=>
+     {dispatch(employeesActions.addEmployee(employee));
+    return true;}}/>
 }
