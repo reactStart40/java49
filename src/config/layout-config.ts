479ee2667ad657ce8d4629e75527dbfb04ab4bcc
Olga Greyskop <o.greyskop@gmail.com>
2023-02-22 07:42:24 +0200

    HW#43 unfinished

--- src/config/layout-config.ts
+++ src/config/layout-config.ts
@@ -2,10 +2,12 @@ import { NavigatorProps } from "../model/NavigatorProps";
 export const layoutConfig: NavigatorProps = {
     className: 'navigator-list',
     routes: [
-        { label: 'Employees', path: '/' },
-        { label: 'Add Employees', path: '/add' },
-        { label: 'Age Statistics', path: '/statistics/age' },
-        { label: 'Salary Statistics', path: '/statistics/salary' }
+        { label: 'Employees', path: '/', flAdmin: false, flAuth: false},
+        { label: 'Add Employees', path: '/add', flAdmin: false, flAuth: false},
+        { label: 'Age Statistics', path: '/statistics/age', flAdmin: false, flAuth: false},
+        { label: 'Salary Statistics', path: '/statistics/salary', flAdmin: false, flAuth: false},
+        { label: 'Login', path: '/login', flAdmin: false, flAuth: false},
+        { label: 'Logout', path: '/ logout', flAdmin: false, flAuth:false}
 
     ]
 }
--- src/model/NavigatorProps.ts
+++ src/model/NavigatorProps.ts
@@ -1,4 +1,6 @@
 export type NavigatorProps = {
     className: string;
-    routes: {path: string, label: string}[];
+    routes: {path: string, label: string, flAdmin: boolean;
+        flAuth: boolean;}[];
 }
+ 
