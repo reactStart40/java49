0ae3a79639a131ae48abeaf376ddba29e0e4dc95
Olga Greyskop <o.greyskop@gmail.com>
2023-03-04 23:43:18 +0200

    HW#46 Done

--- src/components/navigators/NavigatorMobile.tsx
+++ src/components/navigators/NavigatorMobile.tsx
@@ -1,37 +1,31 @@
-import React  from "react";
+import React from "react";
 import { NavigatorProps } from "../../model/NavigatorProps";
-import {Box} from "@mui/system";
+import { Box } from "@mui/system";
 import { Typography } from "@mui/material";
 import MenuIcon from '@mui/icons-material/Menu';
-import { Link, Navigator} from "react-router-dom";
-import { AppBar, IconButton, Toolbar,Drawer, ListItem, List} from '@mui/material';
-
+import { Link, Navigator } from "react-router-dom";
+import { AppBar, IconButton, Toolbar, Drawer, ListItem, List } from '@mui/material';
 
 export const NavigatorMobile: React.FC<NavigatorProps> = ({ routes }) => {
-
-   const [flOpen, setOpen] = React.useState<boolean>(false);
-  
-  
-   function OpenMenuBar() {
-       setOpen(!flOpen);
-   }
-   function getListItems(): React.ReactNode {
-       return routes.map(i => <ListItem onClick={OpenMenuBar} component={Link} to={i.path} key={i.path}>{i.label}</ListItem>)
-   }
-  
-   
-   return <AppBar position="fixed">
-       <Toolbar><IconButton onClick={OpenMenuBar} style={{color: 'white'}}>
-           <MenuIcon/>
-       </IconButton>
-       <Typography >
-       Application
-       </Typography>
-       <Drawer open={flOpen} onClose={OpenMenuBar} anchor="left">
-           <List>
-               {getListItems()}
-           </List>
-       </Drawer></Toolbar>
-   </AppBar>
+    const [flOpen, setOpen] = React.useState<boolean>(false);
+    function OpenMenuBar() {
+        setOpen(!flOpen);
+    }
+    function getListItems(): React.ReactNode {
+        return routes.map(i => <ListItem onClick={OpenMenuBar} component={Link} to={i.path} key={i.path}>{i.label}</ListItem>)
+    }
+    return <AppBar position="fixed">
+        <Toolbar><IconButton onClick={OpenMenuBar} style={{ color: 'white' }}>
+            <MenuIcon />
+        </IconButton>
+            <Typography >
+                Application
+            </Typography>
+            <Drawer open={flOpen} onClose={OpenMenuBar} anchor="left">
+                <List>
+                    {getListItems()}
+                </List>
+            </Drawer></Toolbar>
+    </AppBar>
 }
 export default NavigatorMobile;
