a32f8d64fee6227113fed0b31d621790c379410e
Olga Greyskop <o.greyskop@gmail.com>
2023-03-06 21:56:05 +0200

    HW46 with Location

--- src/components/navigators/NavigatorMobile.tsx
+++ src/components/navigators/NavigatorMobile.tsx
@@ -1,25 +1,39 @@
-import React from "react";
+import React, { useEffect, useState}from 'react';
 import { NavigatorProps } from "../../model/NavigatorProps";
-import { Box } from "@mui/system";
-import { Typography } from "@mui/material";
 import MenuIcon from '@mui/icons-material/Menu';
-import { Link, Navigator } from "react-router-dom";
-import { AppBar, IconButton, Toolbar, Drawer, ListItem, List } from '@mui/material';
+import { Link,  Outlet, useLocation, useNavigate } from "react-router-dom";
+import { AppBar, IconButton, Toolbar, Drawer, ListItem, List, Box, Typography } from '@mui/material';
+
 
 export const NavigatorMobile: React.FC<NavigatorProps> = ({ routes }) => {
-    const [flOpen, setOpen] = React.useState<boolean>(false);
+    const [flOpen, setOpen] = useState<boolean>(false);
+    const location =useLocation();
+    const navigate =useNavigate();
+    useEffect(()=>{
+        if (routes.length >0) {
+            navigate(routes[0].path);
+        }
+        }, [routes]);
+
+        function getTitle():string{
+            const route= routes.find(r => r.path ===location.pathname)
+            return route? route.label : '';
+        }
+
     function OpenMenuBar() {
         setOpen(!flOpen);
     }
     function getListItems(): React.ReactNode {
-        return routes.map(i => <ListItem onClick={OpenMenuBar} component={Link} to={i.path} key={i.path}>{i.label}</ListItem>)
+        return routes.map(i => <ListItem onClick={OpenMenuBar} component={Link} 
+            to={i.path} key={i.path}>{i.label}</ListItem>)
     }
-    return <AppBar position="fixed">
-        <Toolbar><IconButton onClick={OpenMenuBar} style={{ color: 'white' }}>
+    return <Box sx={{marginTop:{xs:"15vh", sm: "20vh"}}}>
+    <AppBar position="fixed">
+        <Toolbar><IconButton onClick={OpenMenuBar} sx={{ color: 'white' }}>
             <MenuIcon />
         </IconButton>
-            <Typography >
-                Application
+            <Typography sx={{width: "100%", textAlign: "center", fontSize: "1,5em"}}>
+                {getTitle()}
             </Typography>
             <Drawer open={flOpen} onClose={OpenMenuBar} anchor="left">
                 <List>
@@ -27,5 +41,6 @@ export const NavigatorMobile: React.FC<NavigatorProps> = ({ routes }) => {
                 </List>
             </Drawer></Toolbar>
     </AppBar>
+    <Outlet></Outlet>
+    </Box>
 }
-export default NavigatorMobile;
